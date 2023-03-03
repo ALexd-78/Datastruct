80a84c2e8db5e2a0d97ea500e65c0f5be740dad2
ALexd-78 <alexd-78@yandex.ru>
2023-03-03 14:59:27 +0300

    Прописал тесты для pop()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -29,11 +29,15 @@ class TestStack(unittest.TestCase):
         stack = Stack()
         stack.push('data1')
         stack.push('data2')
-        # stack.push('data3')
         self.assertEqual(stack.top.data, 'data2')
         self.assertEqual(stack.top.next_node.data, 'data1')
-        # self.assertEqual(stack.top.next_node.next_node.data, 'data1')
 
 
+    def test_pop(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        data = stack.pop()
+        self.assertEqual(stack.top.data, 'data1')
 # if __name__ == '__main__':
 #     unittest.main()
