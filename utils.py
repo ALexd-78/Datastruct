9b9abe02d1783d0707b23a105010fe3031218b16
ALexd-78 <alexd-78@yandex.ru>
2023-03-03 13:26:15 +0300

    Прописал метод pop в utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, data, next_node):
+    def __init__(self, data):
         self.data = data
-        self.next_node = next_node
+        self.next_node = None
 
 class Stack:
 
@@ -11,24 +11,49 @@ class Stack:
 
 
     def push(self, data):
-        next_node = Node(data, self.top)
-        next_node.next = self.top
-        self.top = next_node
+        new_node = Node(data)
+        new_node.next_node = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            return None
+        removed_data = self.top.data
+        self.top = self.top.next_node
+
+        return removed_data
 
 
-n1 = Node(5, None)
-n2 = Node('a', n1)
-print(n1.data)
-print(n2.data)
-print(n1)
-print(n2.next_node)
+stack = Stack()
+stack.push('data1')
+data = stack.pop()
+
+print(stack.top)
+
+print(data)
 
 stack = Stack()
 stack.push('data1')
 stack.push('data2')
-stack.push('data3')
+data = stack.pop()
+
 print(stack.top.data)
-print(stack.top.next_node.data)
-print(stack.top.next_node.next_node.data)
-print(stack.top.next_node.next_node.next_node)
+
+print(data)
+
+# n1 = Node(5, None)
+# n2 = Node('a', n1)
+# print(n1.data)
+# print(n2.data)
+# print(n1)
+# print(n2.next_node)
+
+# stack = Stack()
+# stack.push('data1')
+# stack.push('data2')
+# stack.push('data3')
+# print(stack.top.data)
+# print(stack.top.next_node.data)
+# print(stack.top.next_node.next_node.data)
+# print(stack.top.next_node.next_node.next_node)
 # print(stack.top.next_node.next_node.next_node.data)
