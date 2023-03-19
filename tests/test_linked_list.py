736ba306b722e5dddb7c8109e002db284c5f1b81
ALexd-78 <alexd-78@yandex.ru>
2023-03-19 03:08:51 +0300

    Дополнил тесты для класса LinkedList в test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -2,10 +2,19 @@ import unittest
 
 from linked_list import Node, LinkedList
 
+
+class TestNode(unittest.TestCase):
+    '''Тест для класса Node'''
+    def test_node(self):
+        node = Node(1)
+        self.assertEqual(node.data, 1)
+        self.assertEqual(node.next_node, None)
+
+
 class TestLinkedList(unittest.TestCase):
         '''Тест для класса LinkedList'''
 
-        def test_queue_empty(self):
+        def test_ll_empty(self):
             ll = LinkedList()
             self.assertEqual(ll.head, None)
             self.assertEqual(ll.tail, None)
