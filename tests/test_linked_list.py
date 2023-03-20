46bb875b8080f06c96ea1be7030fbd5ad9aeecb6
ALexd-78 <alexd-78@yandex.ru>
2023-03-20 18:11:36 +0300

    Добавлены тесты для методов класса LinkedList в test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -5,6 +5,7 @@ from linked_list import Node, LinkedList
 
 class TestNode(unittest.TestCase):
     '''Тест для класса Node'''
+
     def test_node(self):
         node = Node(1)
         self.assertEqual(node.data, 1)
@@ -12,11 +13,59 @@ class TestNode(unittest.TestCase):
 
 
 class TestLinkedList(unittest.TestCase):
-        '''Тест для класса LinkedList'''
+    '''Тест для класса LinkedList'''
 
-        def test_ll_empty(self):
-            ll = LinkedList()
-            self.assertEqual(ll.head, None)
-            self.assertEqual(ll.tail, None)
+    def test_ll_empty(self):
+        ll = LinkedList()
+        self.assertEqual(ll.head, None)
+        self.assertEqual(ll.tail, None)
+
+    def test_insert_beginning(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(ll.head.data, {'id': 0})
+        self.assertEqual(ll.head.next_node.data, {'id': 1})
+        self.assertEqual(ll.tail.data, {'id': 1})
+        self.assertEqual(ll.tail.next_node, None)
+
+    def test_insert_at_end(self):
+        ll = LinkedList()
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        self.assertEqual(ll.head.data, {'id': 2})
+        self.assertEqual(ll.head.next_node.data, {'id': 3})
+        self.assertEqual(ll.tail.data, {'id': 3})
+        self.assertEqual(ll.tail.next_node, None)
+
+
+    def test_print_ll(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        self.assertEqual(ll.print_ll(), None)
+
+
+    def test_to_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(ll.to_list(), [{'id': 0}, {'id': 1}, {'id': 2}, {'id': 3}])
+
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(ll.get_data_by_id(2), {'id': 2, 'username': 'mosh_s'})
+
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        # ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end('idusername')
+        # ll.insert_at_end([1, 2, 3])
+        # ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(ll.get_data_by_id(2), 'Данные не являются словарем или в словаре нет id', 'Словарь с id=2 отсутствует')
 
 
